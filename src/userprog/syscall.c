b99ff922973a6d4fe1448c00ae3ebb30290709c5
James Schwinabart <jschwina@vt.edu>
2013-02-21 00:07:16 -0500

    add sys_halt and sys_exit

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,8 +3,13 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
+#include "devices/shutdown.h"
 
 static void syscall_handler (struct intr_frame *);
+
+void sys_halt (void);
+int sys_exit (int status);
 
 void
 syscall_init (void) 
@@ -13,8 +18,36 @@ syscall_init (void)
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler (struct intr_frame *f) 
 {
+  int *p = f->esp;
+  if (!is_user_vaddr (p))
+    sys_exit (-1);
+
+  switch (*p) {
+    case SYS_HALT:
+      sys_halt ();
+      break;
+    case SYS_EXIT:
+      sys_exit (-1);
+      break;
+  }
+
   printf ("system call!\n");
   thread_exit ();
 }
+
+void
+sys_halt (void)
+{
+  shutdown_power_off ();
+}
+
+int
+sys_exit (int status UNUSED)
+{
+  thread_exit ();
+  // TODO: deal with parent processes
+}
+
+// vim:ts=2:sw=2:et:
