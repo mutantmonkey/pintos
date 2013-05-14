81e53fb6faed6dd424f19317fccc8ae7af0a3b74
mutantmonkey <mutantmonkey@mutantmonkey.in>
2013-05-14 15:04:40 -0400

    add hardcoded unsolicited arp transmit

--- netup.sh
+++ netup.sh
@@ -3,7 +3,8 @@
 brctl addbr br0
 brctl addif br0 tap0
 brctl addif br0 enp0s25
-ip addr add 10.177.0.1/24 dev br0
+#ip addr add 10.177.0.1/24 dev br0
+ip addr add 10.0.2.2/24 dev br0
 ip link set tap0 up
 ip link set enp0s25 up
 ip link set br0 up
