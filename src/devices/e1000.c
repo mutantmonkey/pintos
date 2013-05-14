8a37ef5975dd49414bf97c4727aef4df8aedefda
mutantmonkey <mutantmonkey@mutantmonkey.in>
2013-05-13 22:42:26 -0400

    rewrite e1000 driver
    Failed debugging attempts led me to redesign major portions of the e1000
    driver, with limited success, but I thought I'd commit my changes anyway
    since it makes the code significantly more readable.

--- src/devices/e1000.c
+++ src/devices/e1000.c
@@ -8,73 +8,49 @@
 struct tx_desc tx_desc_array[E1000_TXDESC] __attribute__ ((aligned (16)));
 struct tx_pkt tx_pkt_bufs[E1000_TXDESC];
 
-struct rcv_desc rcv_desc_array[E1000_RCVDESC] __attribute__ ((aligned (16)));
-struct rcv_pkt rcv_pkt_bufs[E1000_RCVDESC];
+struct rx_desc rx_desc_array[E1000_RCVDESC] __attribute__ ((aligned (16)));
+struct rx_pkt rx_pkt_bufs[E1000_RCVDESC];
 
 struct pci_io *io;
+
+static void e1000_init_buffers(struct pci_io *io);
+static void e1000_reg_set(struct pci_io *io, uint32_t, uint32_t);
+static void e1000_reg_unset(struct pci_io *io, uint32_t, uint32_t);
 
 void
 e1000_init(void)
 {
-  uint32_t i = 0;
   printf("E1000 start\n");
+
+  // enable PCI device
   struct pci_dev *pci = pci_get_device(E1000_VENDOR, E1000_DEVICE, 0, 0);
   pci_enable(pci);
   
   //struct pci_io *io = pci_io_enum(pci, NULL);
   io = pci_io_enum(pci, NULL);
   printf("%08x \n", pci_reg_read32(io, E1000_STATUS)); 
-  
+
   ASSERT(pci_reg_read32(io, E1000_STATUS) == 0x80080783);
-  memset(tx_desc_array, 0x0, sizeof(struct tx_desc) * E1000_TXDESC);
-  memset(tx_pkt_bufs, 0x0, sizeof(struct tx_pkt) * E1000_TXDESC);
-  for (i = 0; i < E1000_TXDESC; i++) 
-  {
-    tx_desc_array[i].addr = vtop(&tx_pkt_bufs[i].buf);
-    tx_desc_array[i].status |= E1000_TXD_STAT_DD;
-  }
 
-  memset(rcv_desc_array, 0x0, sizeof(struct rcv_desc) * E1000_RCVDESC);
-  memset(rcv_pkt_bufs, 0x0, sizeof(struct rcv_pkt) * E1000_RCVDESC);
-  for (i = 0; i < E1000_RCVDESC; i++) 
-  {
-    rcv_desc_array[i].addr = vtop(&rcv_pkt_bufs[i].buf);
-  }
-  pci_reg_write32(io, E1000_TDBAL, vtop(tx_desc_array)); 
-  pci_reg_write32(io, E1000_TDBAH, 0x0);
-  
+  // initialize as described in section 14.3 of the Intel Gigabit Ethernet
+  // Controllers Software Developer's Manual
+  e1000_reg_set   (io, E1000_CTRL, E1000_CTRL_ASDE | E1000_CTRL_SLU);
+  e1000_reg_unset (io, E1000_CTRL, E1000_CTRL_LRST);
+  e1000_reg_unset (io, E1000_CTRL, E1000_CTRL_PHY_RST);
+  e1000_reg_unset (io, E1000_CTRL, E1000_CTRL_ILOS);
+  pci_reg_write32 (io, E1000_FCAL, 0x0);
+  pci_reg_write32 (io, E1000_FCAH, 0x0);
+  pci_reg_write32 (io, E1000_FCT, 0x0);
+  pci_reg_write32 (io, E1000_FCTTV, 0x0);
+  e1000_reg_unset (io, E1000_CTRL, E1000_CTRL_VME);
 
-  pci_reg_write32(io, E1000_TDLEN, sizeof(struct tx_desc) * E1000_TXDESC);
-
-  pci_reg_write32(io, E1000_TDH, 0x0);
-  pci_reg_write32(io, E1000_TDT, 0x0);
-
-  uint32_t tmp = pci_reg_read32(io, E1000_TCTL);
-  tmp |= E1000_TCTL_EN;
-  tmp |= E1000_TCTL_PSP;
-  tmp &= ~E1000_TCTL_CT;
-  tmp |= (0x10) << 4;
-  tmp &= ~E1000_TCTL_COLD;
-  tmp |= (0x40) << 12;
-  pci_reg_write32(io, E1000_TCTL, tmp);
-
-
-  tmp = pci_reg_read32(io, E1000_TIPG);
-  tmp = 0x0;
-  tmp |= (0x6) << 20; // IPGR2 
-  tmp |= (0x4) << 10; // IPGR1
-  tmp |= 0xA; // IPGR
-  pci_reg_write32(io, E1000_TIPG, tmp);
-
-
-   
+  // set rx address registers
   pci_reg_write32(io, E1000_EERD, 0x0);
   pci_reg_write32(io, E1000_EERD, pci_reg_read32(io, E1000_EERD) | E1000_EERD_START);
   
   while( ! (pci_reg_read32(io, E1000_EERD) & E1000_EERD_DONE));
   pci_reg_write32(io, E1000_RAL, pci_reg_read32(io, E1000_EERD) >> 16);
  
-  
   pci_reg_write32(io, E1000_EERD, 0x1 << 8);
   pci_reg_write32(io, E1000_EERD, pci_reg_read32(io, E1000_EERD) | E1000_EERD_START);	
   
@@ -87,29 +63,100 @@ e1000_init(void)
   while( ! (pci_reg_read32(io, E1000_EERD) & E1000_EERD_DONE));
   pci_reg_write32(io, E1000_RAH, pci_reg_read32(io, E1000_EERD) >> 16);
  
-  pci_reg_write32(io, E1000_RAH, pci_reg_read32(io, E1000_RAH) | (0x1 << 31));
+  pci_reg_write32 (io, E1000_RAL, 0x00f09512);
+  pci_reg_write32 (io, E1000_RAH, 0x5254);
 
-  pci_reg_write32(io, E1000_RDBAL, vtop(rcv_desc_array));
-  pci_reg_write32(io, E1000_RDBAH, 0x0);
-        
-  pci_reg_write32(io, E1000_RDLEN, sizeof(struct rcv_desc) * E1000_RCVDESC);
+  // set address valid bit
+  e1000_reg_set   (io, E1000_RAH, E1000_RAH_AV);
 
-  pci_reg_write32(io, E1000_RDH, 0x0);
-  pci_reg_write32(io, E1000_RDT, 0x0);
+  // enable multicast promiscuous mode
+  e1000_reg_set   (io, E1000_RCTL, E1000_RCTL_MPE);
 
-  tmp = pci_reg_read32(io, E1000_RCTL);
-  tmp |= E1000_RCTL_EN;
-  tmp &= ~E1000_RCTL_LPE;
-  tmp &= ~E1000_RCTL_LBM;
-  tmp &= ~E1000_RCTL_RDMTS;
-  tmp &= ~E1000_RCTL_MO;
-  tmp |= E1000_RCTL_BAM;
-  tmp &= ~E1000_RCTL_SZ; // 2048 byte size
-  tmp |= E1000_RCTL_SECRC;
-  pci_reg_write32(io, E1000_RCTL, tmp);
- 
+  e1000_init_buffers (io);
+
+  // set tx IPG register
+  pci_reg_write32 (io, E1000_TIPG, 0x0);
+  e1000_reg_set   (io, E1000_TIPG, (0x6) << 20); // IPGR2
+  e1000_reg_set   (io, E1000_TIPG, (0x4) << 10); // IPGR1
+  e1000_reg_set   (io, E1000_TIPG, 0xA);         // IPGR
 }
 
+static void
+e1000_init_buffers(struct pci_io *io)
+{
+  uint32_t i = 0;
+
+  // initialize rx buffer array
+  memset(rx_desc_array, 0x0, sizeof(struct rx_desc) * E1000_RCVDESC);
+  memset(rx_pkt_bufs, 0x0, sizeof(struct rx_pkt) * E1000_RCVDESC);
+  for (i = 0; i < E1000_RCVDESC; i++) 
+  {
+    rx_desc_array[i].buffer = vtop(&rx_pkt_bufs[i].buf);
+    rx_desc_array[i].status = 0;
+  }
+
+  // initialize tx buffer array
+  memset(tx_desc_array, 0x0, sizeof(struct tx_desc) * E1000_TXDESC);
+  memset(tx_pkt_bufs, 0x0, sizeof(struct tx_pkt) * E1000_TXDESC);
+  for (i = 0; i < E1000_TXDESC; i++) 
+  {
+    tx_desc_array[i].buffer = vtop(&tx_pkt_bufs[i].buf);
+    tx_desc_array[i].status = E1000_TXD_STAT_DD;
+  }
+
+  // setup rx ring registers
+  pci_reg_write32 (io, E1000_RDBAL, vtop (rx_desc_array));
+  pci_reg_write32 (io, E1000_RDBAH, 0x0);
+  pci_reg_write32 (io, E1000_RDLEN, sizeof(struct rx_desc) * E1000_RCVDESC);
+  pci_reg_write32 (io, E1000_RDH, 0x0);
+  pci_reg_write32 (io, E1000_RDT, 0x0);
+  e1000_reg_unset (io, E1000_RCTL, E1000_RCTL_SZ);
+  e1000_reg_set   (io, E1000_RCTL, E1000_RCTL_EN);
+
+  // set RCTL bits (same as used in JOS)
+  e1000_reg_unset (io, E1000_RCTL, E1000_RCTL_LPE);
+  e1000_reg_unset (io, E1000_RCTL, E1000_RCTL_LBM);
+  e1000_reg_unset (io, E1000_RCTL, E1000_RCTL_RDMTS);
+  e1000_reg_unset (io, E1000_RCTL, E1000_RCTL_MO);
+  e1000_reg_set   (io, E1000_RCTL, E1000_RCTL_BAM);
+  e1000_reg_set   (io, E1000_RCTL, E1000_RCTL_SECRC);
+
+  // setup tx ring registers
+  pci_reg_write32 (io, E1000_TDBAL, vtop (tx_desc_array));
+  pci_reg_write32 (io, E1000_TDBAH, 0x0);
+  pci_reg_write32 (io, E1000_TDLEN, sizeof(struct tx_desc) * E1000_TXDESC);
+  pci_reg_write32 (io, E1000_TDH, 0x0);
+  pci_reg_write32 (io, E1000_TDT, 0x0);
+  e1000_reg_set   (io, E1000_TCTL, E1000_TCTL_EN | E1000_TCTL_PSP);
+
+  // set TCTL bits (same as used by JOS)
+  e1000_reg_unset (io, E1000_TCTL, E1000_TCTL_CT);
+  e1000_reg_set   (io, E1000_TCTL, (0x10) << 4);
+  e1000_reg_unset (io, E1000_TCTL, E1000_TCTL_COLD);
+  e1000_reg_set   (io, E1000_TCTL, (0x40) << 12);
+}
+
+/* Set the value of an e1000 register.
+   Inspired by the helper function in Minix. */
+static void
+e1000_reg_set(struct pci_io *io, uint32_t reg, uint32_t value)
+{
+  uint32_t data;
+
+  data = pci_reg_read32 (io, reg);
+  pci_reg_write32 (io, reg, data | value);
+}
+
+/* Unset the value of an e1000 register.
+   Inspired by the helper function in Minix. */
+static void
+e1000_reg_unset(struct pci_io *io, uint32_t reg, uint32_t value)
+{
+  uint32_t data;
+
+  data = pci_reg_read32 (io, reg);
+  pci_reg_write32 (io, reg, data & ~value);
+}
 
 int
 e1000_transmit(char *data, int len)
@@ -128,8 +175,8 @@ e1000_transmit(char *data, int len)
     //HEXDUMP("tx dump:", data, len);
 
     tx_desc_array[tdt].status &= ~E1000_TXD_STAT_DD;
-    tx_desc_array[tdt].cmd |= E1000_TXD_CMD_RS;
-    tx_desc_array[tdt].cmd |= E1000_TXD_CMD_EOP;
+    tx_desc_array[tdt].command |= E1000_TXD_CMD_RS;
+    tx_desc_array[tdt].command |= E1000_TXD_CMD_EOP;
 
     //e1000[E1000_TDT] = (tdt + 1) % E1000_TXDESC;
     pci_reg_write32(io, E1000_TDT, (tdt + 1) % E1000_TXDESC);
@@ -147,29 +194,24 @@ int
 e1000_receive(char *data)
 {
   uint32_t rdt, len;
-  //rdt = e1000[E1000_RDT];
+  rdt = pci_reg_read32 (io, E1000_RDT);
 
-  rdt = pci_reg_read32(io, E1000_RDT);
-  if (rcv_desc_array[rdt].status & E1000_RXD_STAT_DD) 
+  if (rx_desc_array[rdt].status & E1000_RXD_STAT_DD) 
   {
-    if (!(rcv_desc_array[rdt].status & E1000_RXD_STAT_EOP)) 
+    if (!(rx_desc_array[rdt].status & E1000_RXD_STAT_EOP)) 
     {
       PANIC("Don't allow jumbo frames!\n");
     }
-    len = rcv_desc_array[rdt].length;
+    len = rx_desc_array[rdt].length;
 
-    memmove(data, rcv_pkt_bufs[rdt].buf, len);
-    hex_dump(data, data, len, true);
-    rcv_desc_array[rdt].status &= ~E1000_RXD_STAT_DD;
-    rcv_desc_array[rdt].status &= ~E1000_RXD_STAT_EOP;
+    memmove(data, rx_pkt_bufs[rdt].buf, len);
+    hex_dump(0, data, len, true);
+    rx_desc_array[rdt].status &= ~E1000_RXD_STAT_DD;
+    rx_desc_array[rdt].status &= ~E1000_RXD_STAT_EOP;
     //e1000[E1000_RDT] = (rdt + 1) % E1000_RCVDESC;
     pci_reg_write32(io, E1000_RDT, (rdt + 1) % E1000_RCVDESC);
     return len;
   }
-  else {
-      printf("%x\n", rcv_desc_array[rdt].status);
-      printf("%x\n", rdt);
 
-  }
   return -E_RCV_EMPTY;
 }
--- src/devices/e1000.h
+++ src/devices/e1000.h
@@ -11,11 +11,18 @@
 #define RCV_PKT_SIZE 2048
 
 // Register Set
+#define E1000_CTRL     0x00000      /* Device Control */
+
 #define E1000_STATUS   0x00008///4  /* Device Status - RO */
 #define E1000_EERD     0x00014///4  /* EEPROM Read - RW */
 
 #define E1000_EERD_START 0x01
 #define E1000_EERD_DONE  0x10
+
+#define E1000_FCAL     0x00028      /* Flow Control Address Low */
+#define E1000_FCAH     0x0002c      /* Flow Control Address High */
+#define E1000_FCT      0x00030      /* Flow Control Type */
+#define E1000_FCTTV    0x00170      /* Flow Control Transmit Timer Value */
 
 #define E1000_TDBAL    0x03800///4  /* TX Descriptor Base Address Low - RW */
 #define E1000_TDBAH    0x03804///4  /* TX Descriptor Base Address High - RW */
@@ -39,6 +46,8 @@
 
 #define E1000_RCTL     0x00100///4  /* RX Control - RW */
 #define E1000_RCTL_EN             0x00000002    /* enable */
+#define E1000_RCTL_UPE            0x00000004    /* Unicast Promiscuous Enable */
+#define E1000_RCTL_MPE            0x00000008    /* Multicast Promiscuous Enable */
 #define E1000_RCTL_LPE            0x00000020    /* long packet enable */
 #define E1000_RCTL_LBM            0x000000C0    /* loopback mode */
 #define E1000_RCTL_RDMTS          0x00000300    /* rx min threshold size */
@@ -49,6 +58,15 @@
 
 #define E1000_TIPG     0x00410///4  /* TX Inter-packet gap -RW */
 
+/* Control Register bit defintions */
+#define E1000_CTRL_ASDE      (1 << 5)   /* Auto-Speed Detection Enable */
+#define E1000_CTRL_LRST      (1 << 3)   /* Link Reset */
+#define E1000_CTRL_SLU       (1 << 6)   /* Set Link Up */
+#define E1000_CTRL_ILOS      (1 << 7)   /* Invert Loss-of-Signal */
+#define E1000_CTRL_RST       (1 << 26)  /* Device Reset */
+#define E1000_CTRL_VME       (1 << 30)  /* VLAN Mode Enable */
+#define E1000_CTRL_PHY_RST   (1 << 31)  /* PHY Reset */
+
 /* Transmit Descriptor bit definitions */
 #define E1000_TXD_CMD_RS     0x00000008 /* Report Status */
 #define E1000_TXD_CMD_EOP    0x00000001 /* End of Packet */
@@ -58,6 +76,9 @@
 #define E1000_RXD_STAT_DD       0x01    /* Descriptor Done */
 #define E1000_RXD_STAT_EOP      0x02    /* End of Packet */
 
+/* Receive Address High bit defintions */
+#define E1000_RAH_AV            (1 << 31)   /* Receive Address Valid */
+
 enum
 {
   //E1000 error codes
@@ -65,7 +86,8 @@ enum
   E_RCV_EMPTY     = 17,   // Receive queue is empty
   E_PKT_TOO_LONG  = 18   // Packet size is too big
 };
-struct tx_desc
+
+/*struct tx_desc
 {
 	uint64_t addr;
 	uint16_t length;
@@ -76,7 +98,7 @@ struct tx_desc
 	uint16_t special;
 } __attribute__((packed));
 
-struct rcv_desc
+struct rx_desc
 {
 	uint64_t addr;
 	uint16_t length;
@@ -84,14 +106,35 @@ struct rcv_desc
 	uint8_t status;
 	uint8_t errors;
 	uint16_t special;
-} __attribute__((packed));
+} __attribute__((packed));*/
+
+struct rx_desc
+{
+    uint64_t buffer;
+    uint16_t length;
+    uint16_t checksum;
+    uint8_t  status;
+    uint8_t  errors;
+    uint16_t special;
+};
+
+struct tx_desc
+{
+    uint64_t buffer;
+    uint16_t length;
+    uint8_t  checksum_off;
+    uint8_t  command;
+    uint8_t  status;
+    uint8_t  checksum_st;
+    uint16_t special;
+};
 
 struct tx_pkt
 {
 	uint8_t buf[TX_PKT_SIZE];
 } __attribute__((packed));
 
-struct rcv_pkt
+struct rx_pkt
 {
 	uint8_t buf[RCV_PKT_SIZE];
 } __attribute__((packed));
